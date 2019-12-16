92e76476a23c8b96b183ab6b6f962a579db47fb9
Matt Dee <mdee@hioscar.com>
2019-12-15 19:45:31 -0500

    fix scripts

--- scripts/metar_cron.sh
+++ scripts/metar_cron.sh
@@ -5,9 +5,9 @@ DATETIME="$(date +'%Y_%m_%d_%H%M')"
 DATE="$(date +'%Y_%m_%d')"
 
 LOG_DIR="/home/mattdee/aviationweather/log/$DATE"
-mkdir -p $LOGDIR
-LOG_FILE="$LOGDIR/$DATETIME.log"
-ERR_FILE="$LOGDIR/$DATETIME.err"
+mkdir -p $LOG_DIR
+LOG_FILE="$LOG_DIR/$DATETIME.log"
+ERR_FILE="$LOG_DIR/$DATETIME.err"
 TMP_FILE="/home/mattdee/aviationweather/log/files/$DATETIME.csv"
 
-/home/mattdee/aviationweather/dist/metar_scraper --dburl 'host=/run/postgresql dbname=mattdee sslmode=disable' --filename /home/mattdee/fname --download > $LOG_FILE 2>> $ERR_FILE
+/home/mattdee/aviationweather/dist/metar_scraper --dburl 'host=/run/postgresql dbname=mattdee sslmode=disable' --filename $TMP_FILE --download > $LOG_FILE 2>> $ERR_FILE
